fe4810d72085ea5a1718ea36261318e9bbe8289b
Rachel Killackey <rachel.killackey@homechef.com>
2019-03-14 16:52:26 -0500

    Make divvy request, render stations...in a bad way :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stations: []
+    };
+  }
+
+  getDivvyBikes = () => {
+    axios.get("/divvy").then(res => this.setState({ stations: res.data }));
+  };
+
+  componentDidMount() {
+    this.getDivvyBikes();
+    setInterval(this.getDivvyBikes, 5000);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>TEAM GO HOME PRESENTS!</h1>
+          <h2>Divvy Bikes No One Takes</h2>
+          <p>But seriously...these updates are faster than the Divvy web app</p>
+          <table>
+            <tbody>
+              {this.state.stations.map((station, idx) => (
+                <tr key={idx} style={{ borderBottom: "4px solid white" }}>
+                  <td style={{ textAlign: "left", paddingRight: "500px" }}>
+                    {station.name}
+                  </td>
+                  <td style={{ textAlign: "right" }}>
+                    {station.available_bikes} bikes
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </header>
       </div>
     );
